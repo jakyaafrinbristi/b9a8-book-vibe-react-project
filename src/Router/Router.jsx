7d8630be7efd45cd77ddd27d3d10c394bf822569
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-03-27 21:06:06 +0600

    json-data-created

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,20 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home/Home";
+
+import Root from "../layout/Root";
 import ListedBooks from "../pages/ListedBooks/ListedBooks";
 import PagesRead from "../pages/PagesRead/PagesRead";
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element:<Home></Home> ,
-    },
-    {
-      path: "/listed",
-      element: <ListedBooks></ListedBooks>,
-    },
-    {
+      element:<Root></Root> ,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:[
+        {
+            path: "/",
+            element: <Home></Home>
+        },
+        {
+            path: "/listed",
+            element: <ListedBooks></ListedBooks>,
+        },
+         {
       path: "/read",
       element: <PagesRead></PagesRead>
     },
+      ]
+    }
+   
   ]);
   export default router;
